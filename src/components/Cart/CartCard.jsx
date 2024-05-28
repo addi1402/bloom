460d6b96cff0ab3326986109a5e5c2015b784a3c
addi1402 <addi1402@gmail.com>
2024-05-28 17:26:24 +0530

    Add cart to product page functionality

--- src/components/Cart/CartCard.jsx
+++ src/components/Cart/CartCard.jsx
@@ -5,31 +5,34 @@ import { LuTrash2 } from "react-icons/lu";
 import { useDispatch } from "react-redux";
 import { removeFromCart } from "@/redux/slices/productSlice";
 import { toast } from "../ui/use-toast";
+import Link from "next/link";
 
 export default function CartCard({ item }) {
   const dispatch = useDispatch();
   return (
     <div className="flex flex-col md:flex-row justify-between w-full mb-1 p-4 border rounded-md">
-      <div className="flex gap-4 w-full md:w-3/4">
-        <Image
-          src={"/placeholder.svg"}
-          height="100"
-          width="100"
-          className="w-24 h-24 md:w-32 md:h-32 object-cover rounded-md"
-          alt="Product Placeholder Image"
-        />
-        <div className="flex flex-col justify-between w-full">
-          <div>
-            <h3 className="font-bold capitalize">
-              {item.productName ?? "Unknown Product"}
-            </h3>
-            <p className="capitalize text-sm text-slate-500">
-              Sold By: {item.name ?? "Anonymous"}
-            </p>
+      <Link href={`/product/${item.id}`}>
+        <div className="flex gap-4 w-full md:w-3/4">
+          <Image
+            src={item?.image ?? "/placeholder.svg"}
+            height="100"
+            width="100"
+            className="w-24 h-24 md:w-32 md:h-32 object-cover rounded-md"
+            alt="Product Placeholder Image"
+          />
+          <div className="flex flex-col justify-between w-full">
+            <div>
+              <h3 className="font-bold capitalize">
+                {item.productName ?? "Unknown Product"}
+              </h3>
+              <p className="capitalize text-sm text-slate-500">
+                Sold By: {item.name ?? "Anonymous"}
+              </p>
+            </div>
+            <p className="font-bold text-lg">${item.productPrice ?? "N/A"}</p>
           </div>
-          <p className="font-bold text-lg">${item.productPrice ?? "N/A"}</p>
         </div>
-      </div>
+      </Link>
       <div className="flex items-center justify-end mt-4 md:mt-0 md:w-1/4">
         <CustomButton
           text="Remove"
